061f97a180b4f684123d4b9227a2b623152c7384
sambi <sambireddy7726@gmail.com>
2026-04-09 19:04:41 +0530

    metup

--- 02-station.sh
+++ 02-station.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh:: how are you"
-echo "Ramesh::how was your day"
-echo "Ravi:: i am good"
-echo "ravi :: its going good nd happy to met you after lonh time"
+PERSON1=Aanvi
+PERSON2=Madhavi
+
+echo "$PERSON1:: how are you $PERSON2"
+echo "$PERSON1::how was your day"
+echo "$PERSON2:: i am good $PERSON1"
+echo "$PERSON2 :: its going good nd happy to met you after lonh time"
 
